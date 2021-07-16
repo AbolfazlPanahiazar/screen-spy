bc259670a12a79e28cf1d63e3f998e11ea5d4da2
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2021-07-16 15:19:26 +0430

    Add loop and sleep time

--- main.go
+++ main.go
@@ -3,17 +3,26 @@ package main
 import (
 	"image/png"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/vova616/screenshot"
 )
 
 func main() {
+	for i := 0; i < 10; i++ {
+		captureScreen()
+		time.Sleep(time.Minute)
+	}
+}
+
+func captureScreen() {
 	img, err := screenshot.CaptureScreen()
 	if err != nil {
 		panic(err)
 	}
 
-	f, err := os.Create("./ss.png")
+	f, err := os.Create("./" + strconv.FormatInt(time.Now().UTC().UnixNano(), 10) + ".png")
 	if err != nil {
 		panic(err)
 	}
